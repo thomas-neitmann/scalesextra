1cb74f55679d1d2c9bd30aa500f6f9b01f887401
Thomas Neitmann <th.neitmann@gmail.com>
2020-05-03 11:00:52 +0200

    Add suffix for large numbers

--- R/scale_pretty.R
+++ R/scale_pretty.R
@@ -9,7 +9,17 @@ scale_y_pretty <- function(...) {
 }
 
 pretty <- function(x) {
-  prettyNum(x, big.mark = ",", decimal.mark = ".", scientific = FALSE)
+  max_x <- max(x, na.rm = TRUE)
+  f <- function(x) {
+    format(x, big.mark = ",", decimal.mark = ".", scientific = FALSE)
+  }
+  if (max_x >= 1e9) {
+    return(paste(f(x / 1e9), "Bn."))
+  } else if (max_x >= 1e6) {
+    return(paste(f(x / 1e6), "Mn."))
+  } else {
+    return(f(x))
+  }
 }
 
 #' @export
@@ -22,6 +32,16 @@ scale_y_pretty2 <- function(...) {
   ggplot2::scale_y_continuous(..., labels = pretty2)
 }
 
-pretty2 <- function(x, digits = 0) {
-  prettyNum(x, big.mark = " ", decimal.mark = ",", scientific = FALSE)
+pretty2 <- function(x) {
+  max_x <- max(x, na.rm = TRUE)
+  f <- function(x) {
+    format(x, big.mark = ",", decimal.mark = ".", scientific = FALSE)
+  }
+  if (max_x >= 1e9) {
+    return(paste(f(x / 1e9), "Bn."))
+  } else if (max_x >= 1e6) {
+    return(paste(f(x / 1e6), "Mn."))
+  } else {
+    return(f(x))
+  }
 }
