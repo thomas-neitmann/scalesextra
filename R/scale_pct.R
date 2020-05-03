74eba62aac942352090edfe2574c279357111b02
Thomas Neitmann <th.neitmann@gmail.com>
2020-05-03 11:22:08 +0200

    Move logic inside the `pct` function

--- R/scale_pct.R
+++ R/scale_pct.R
@@ -1,62 +1,32 @@
 #' @export
 scale_x_pct <- function(...) {
-  args <- list(...)
-  args$axis <- "x"
-  structure(
-    args,
-    class = "pct_scale"
-  )
+  ggplot2::scale_x_continuous(..., labels = pct)
 }
 
 #' @export
 scale_y_pct <- function(...) {
-  args <- list(...)
-  args$axis <- "y"
-  structure(
-    args,
-    class = "pct_scale"
-  )
+  ggplot2::scale_y_continuous(..., labels = pct)
 }
 
-#' @export
-ggplot_add.pct_scale <- function(object, plot, object_name) {
-  data <- plot[["data"]]
-  var <- plot[["mapping"]][[object$axis]]
-  if (object$axis == "x") {
-    scale <- ggplot2::scale_x_continuous
-  } else {
-    scale <- ggplot2::scale_y_continuous
-  }
-  r <- range(rlang::eval_tidy(var, data), na.rm = TRUE)
-
+pct <- function(x) {
+  r <- range(x, na.rm = TRUE)
   if (r[1] < 0 || r[2] > 100) {
     rlang::abort("Values must be in [0, 1] or [0, 100].")
   }
 
-  if (exists("breaks", object)) {
-    if (r[2] <= 1) {
-      breaks <- object[["breaks"]] * 100
-    } else {
-      breaks <- object[["breaks"]]
-    }
+  if (r[2] <= 1) {
+    breaks <- x * 100
   } else {
-    if (r[2] <= 1) {
-      breaks <- labeling::extended(r[1] * 100, r[2] * 100, m = 5)
-    } else {
-      breaks <- labeling::extended(r[1], r[2], m = 5)
-    }
+    breaks <- x
   }
 
   accuracy <- 1 / (10^max_n_digits(breaks))
-
-  args <- object[names(object) != "axis"]
   if (r[2] <= 1) {
-    args$labels <- scales::percent_format(accuracy, 100)
+    f <- scales::percent_format(accuracy, 100)
   } else {
-    args$labels <- scales::percent_format(accuracy, 1)
+    f <- scales::percent_format(accuracy, 1)
   }
-
-  plot + do.call(scale, args)
+  f(x)
 }
 
 max_n_digits <- function(x) {
